1b52c2c5601bf6cd06e5c7c1de21293cf077e22c
Steva <asteva@hotmail.co.uk>
2023-02-18 17:02:42 +0000

    Added basic code for components

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Jumbotron from "../Jumbotron/Jumbotron";
+import Footer from "../Footer/Footer";
+function Home() {
+    return (
+        <div>
+            <Navbar />
+            <Jumbotron/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default Home;
