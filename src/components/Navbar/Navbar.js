ed413f86cc4f76092197b7f7360974533be1a4ee
Steva <asteva@hotmail.co.uk>
2023-04-11 17:38:35 +0100

    edited colour scheme

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,26 @@
 import React from "react";
 import style from "./Navbar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import {faPhone} from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+library.add(faHouse);
  
 function Navbar () { 
     return ( 
 <nav className="navbar navbar-expand-lg  bg-light">
   <div className="container-fluid nav-content">
-    <img className="navbar-brand icon" src={require ("../../assets/avatar.png")} ></img>
+    {/* <img className="navbar-brand icon" src={require ("../../assets/avatar.png")} ></img> */}
 
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse navsec" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Projects</a>
-        <a className="nav-link" href="#">Contact</a>
+        <a className="nav-link active" aria-current="page" href="#"><FontAwesomeIcon icon="house" />Home</a>
+        <a className="nav-link" href="#"><FontAwesomeIcon icon={faPenToSquare} />Projects</a>
+        <a className="nav-link" href="#"><FontAwesomeIcon icon={faPhone} />Contact</a>
       </div>
     </div>
   </div>
