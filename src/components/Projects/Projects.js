e60c8592b06651bd856ff477b572398263a3b4a4
Steva <asteva@hotmail.co.uk>
2023-04-27 14:30:36 +0100

    edited projects page layout

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,6 +9,9 @@ function Projects() {
         <div className="body">
             <Navbar />
             <div className="main-content">
+                <div className="projectsTitle">
+                    <h1> My Projects</h1>
+                </div>
                 <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
                     <div className="carousel-indicators">
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -16,25 +19,25 @@ function Projects() {
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                     </div>
                     <div className="carousel-inner">
-                        <div className="carousel-item active" data-bs-interval="10000" >
+                        <div className="carousel-item active placeholderContain" data-bs-interval="10000" >
                             <img src={placeholder} className="d-block w-100" alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
+                            <div className="carousel-caption d-none d-md-block placeholder ">
+                                <h5>Coming soon</h5>
+                                <p>Coming soon</p>
                             </div>
                         </div>
                         <div className="carousel-item" data-bs-interval="10000" >
                             <img src={placeholder} className="d-block w-100" alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>Second slide label</h5>
-                                <p>Some representative placeholder content for the second slide.</p>
+                            <div className="carousel-caption d-none d-md-block placeholder">
+                                <h5>Coming soon</h5>
+                                <p>Coming soon</p>
                             </div>
                         </div>
-                        <div className="carousel-item" data-bs-interval="10000" >
+                        <div className="carousel-item " data-bs-interval="10000" >
                             <img src={placeholder} className="d-block w-100" alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>Third slide label</h5>
-                                <p>Some representative placeholder content for the third slide.</p>
+                            <div className="carousel-caption d-none d-md-block placeholder">
+                                <h5>Coming soon</h5>
+                                <p>Coming soon</p>
                             </div>
                         </div>
                     </div>
