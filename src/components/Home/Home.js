b53057f6bd7b3c6acf08dbac4c5e56db3de8f5a7
Steva <asteva@hotmail.co.uk>
2023-04-27 16:46:27 +0100

    linked home button to projects

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,17 +3,21 @@ import Navbar from "../Navbar/Navbar";
 import Jumbotron from "../Jumbotron/Jumbotron";
 import Footer from "../Footer/Footer";
 import style from "./Home.css";
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
         <div className="body">
             <Navbar />
-            <div className="main-content"> 
-            <div className="bodyimage"> </div>
-            <button type="button" className="btn btn-primary btn-lg">Large button</button>
+            <div className="main-content">
+                <div className="bodyimage"> </div>
+                <Link to="/projects">
+                    <button type="button" className="btn btn-primary btn-lg">Click here for projects</button>
+                </Link>
             </div>
-            <Footer/> 
+            <Footer />
         </div>
     );
-} 
+}
 
 export default Home;
